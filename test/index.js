ef41509e4e3b586167bee3008456660bf502c51f
keith-lin <keith.lin2009@gmail.com>
2024-07-08 14:24:46 +0800

    test demo6
    test demo6

--- test/index.js
+++ test/index.js
@@ -1,119 +1,129 @@
-// Reference from https://github.com/josdirksen/learning-threejs/blob/master/chapter-02/07-both-cameras.html
+import { OrbitControls } from '../lib/OrbitControls.js'
 
-// once everything is loaded, we run our Three.js stuff.
-function init() {
-    // create a scene, that will hold all our elements such as objects, cameras and lights.
-    var scene = new THREE.Scene()
-  
-    // create a camera, which defines where we're looking at.
-    var camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
-    camera.position.x = 120
-    camera.position.y = 60
-    camera.position.z = 180
-  
-    // create a render and set the size
-    var renderer = new THREE.WebGLRenderer()
-  
-    renderer.setClearColor(0xeeeeee, 1.0)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-  
-    // create the ground plane
-    var planeGeometry = new THREE.PlaneGeometry(180, 180)
-    var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial)
-  
-    // rotate and position the plane
-    plane.rotation.x = -0.5 * Math.PI
-    plane.position.x = 0
-    plane.position.y = 0
-    plane.position.z = 0
-  
-    // add the plane to the scene
-    scene.add(plane)
-  
-    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
-  
-    for (var j = 0; j < planeGeometry.parameters.height / 5; j++) {
-      for (var i = 0; i < planeGeometry.parameters.width / 5; i++) {
-        var rnd = Math.random() * 0.75 + 0.25
-        var cubeMaterial = new THREE.MeshLambertMaterial()
-        cubeMaterial.color = new THREE.Color(rnd, 0, 0)
-        var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-  
-        cube.position.z = -(planeGeometry.parameters.height / 2) + 2 + j * 5
-        cube.position.x = -(planeGeometry.parameters.width / 2) + 2 + i * 5
-        cube.position.y = 2
-  
-        scene.add(cube)
-      }
-    }
-  
-    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.7)
-    directionalLight.position.set(-20, 40, 60)
-    scene.add(directionalLight)
-  
-    // add subtle ambient lighting
-    var ambientLight = new THREE.AmbientLight(0x292929)
-    scene.add(ambientLight)
-  
-    // add the output of the renderer to the html element
-    document.body.appendChild(renderer.domElement)
-  
-    // call the render function
-    var step = 0
-  
-    var controls = new function() {
-      this.cameraType = 'Perspective'
-      this.switchCamera = function() {
-        if (camera instanceof THREE.PerspectiveCamera) {
-          camera = new THREE.OrthographicCamera(
-            window.innerWidth / -16,
-            window.innerWidth / 16,
-            window.innerHeight / 16,
-            window.innerHeight / -16,
-            -200,
-            500
-          )
-          camera.position.x = 120
-          camera.position.y = 60
-          camera.position.z = 180
-          camera.lookAt(scene.position)
-          this.cameraType = 'Orthographic'
-        } else {
-          camera = new THREE.PerspectiveCamera(
-            45,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            1000
-          )
-          camera.position.x = 120
-          camera.position.y = 60
-          camera.position.z = 180
-  
-          camera.lookAt(scene.position)
-          this.cameraType = 'Perspective'
-        }
-      }
-    }()
-  
-    var gui = new dat.GUI()
-    gui.add(controls, 'switchCamera')
-    gui.add(controls, 'cameraType').listen()
-  
-    // make sure that for the first time, the
-    // camera is looking at the scene
-    camera.lookAt(scene.position)
-    render()
-  
-    function render() {
-      // render using requestAnimationFrame
-      requestAnimationFrame(render)
-      renderer.render(scene, camera)
-    }
+let renderer, scene, camera
+let cameraControl, stats
+
+// 苦力怕物件
+class Creeper {
+  constructor() {
+    // 宣告頭、身體、腳幾何體大小
+    const headGeo = new THREE.BoxGeometry(4, 4, 4)
+    const bodyGeo = new THREE.BoxGeometry(4, 8, 2)
+    const footGeo = new THREE.BoxGeometry(2, 3, 2)
+
+    // 馮氏材質設為綠色
+    const creeperMat = new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+
+    // 頭
+    this.head = new THREE.Mesh(headGeo, creeperMat)
+    this.head.position.set(0, 6, 0)
+
+    // 身體
+    this.body = new THREE.Mesh(bodyGeo, creeperMat)
+    this.body.position.set(0, 0, 0)
+
+    // 四隻腳
+    this.foot1 = new THREE.Mesh(footGeo, creeperMat)
+    this.foot1.position.set(-1, -5.5, 2)
+    this.foot2 = this.foot1.clone() // 剩下三隻腳都複製第一隻的 Mesh
+    this.foot2.position.set(-1, -5.5, -2)
+    this.foot3 = this.foot1.clone()
+    this.foot3.position.set(1, -5.5, 2)
+    this.foot4 = this.foot1.clone()
+    this.foot4.position.set(1, -5.5, -2)
+
+    // 將四隻腳組合為一個 group
+    this.feet = new THREE.Group()
+    this.feet.add(this.foot1)
+    this.feet.add(this.foot2)
+    this.feet.add(this.foot3)
+    this.feet.add(this.foot4)
+
+    // 將頭、身體、腳組合為一個 group
+    this.creeper = new THREE.Group()
+    this.creeper.add(this.head)
+    this.creeper.add(this.body)
+    this.creeper.add(this.feet)
   }
-  window.onload = init
+}
+
+// 生成苦力怕並加到場景
+function createCreeper() {
+  const creeperObj = new Creeper()
+  scene.add(creeperObj.creeper)
+}
+
+function initStats() {
+  const stats = new Stats()
+  stats.setMode(0)
+  document.getElementById('stats').appendChild(stats.domElement)
+  return stats
+}
+
+// 畫面初始化
+function init() {
+  scene = new THREE.Scene()
+
+  // 相機設定
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+  camera.position.set(30, 30, 30)
+  camera.lookAt(scene.position)
+
+  // 三軸座標輔助
+  let axes = new THREE.AxesHelper(20)
+  scene.add(axes)
+
+  stats = initStats()
+
+  // 渲染器設定
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  // 建立 OrbitControls
+  cameraControl = new OrbitControls(camera, renderer.domElement)
+  cameraControl.enableDamping = true // 啟用阻尼效果
+  cameraControl.dampingFactor = 0.25 // 阻尼系數
+  // cameraControl.autoRotate = true // 啟用自動旋轉
+
+  // 簡單的地板
+  const planeGeometry = new THREE.PlaneGeometry(60, 60)
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
+  let plane = new THREE.Mesh(planeGeometry, planeMaterial)
+  plane.rotation.x = -0.5 * Math.PI
+  plane.position.set(0, -7, 0)
+  scene.add(plane)
+
+  // 產生苦力怕物件並加到場景
+  createCreeper()
+
+  // 簡單的 spotlight 照亮物體
+  let spotLight = new THREE.SpotLight(0xffffff)
+  spotLight.position.set(-10, 40, 30)
+  scene.add(spotLight)
+  // let spotHelper = new THREE.SpotLightHelper(spotLight)
+  // scene.add(spotHelper)
+
+  // 將渲染出來的畫面放到網頁上的 DOM
+  document.body.appendChild(renderer.domElement)
+}
+
+function render() {
+  stats.update()
+  cameraControl.update()
+  requestAnimationFrame(render)
+  renderer.render(scene, camera)
+}
+
+window.addEventListener('resize', function() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+init()
+render()
